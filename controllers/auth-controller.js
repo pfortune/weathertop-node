448ca6c2d6c0d20c1c2421c7aaaff53d140d1bc8
Peter Fortune <pfortune@gmail.com>
2023-07-28 23:02:36 +0100

    Added beginning of AuthController, Register and Login, Updated Menu

---
+++ controllers/auth-controller.js
@@ -0,0 +1,21 @@
+export const authController = {
+  async login(request, response) {
+    const viewData = {
+      title: "Login",
+      flash: request.flash,
+    };
+
+    console.log("-- login rendered");
+    response.render("login-view", viewData);
+  },
+
+  async register(request, response) {
+    const viewData = {
+      title: "Register",
+      flash: request.flash,
+    };
+
+    console.log("-- register rendered");
+    response.render("register-view", viewData);
+  }
+};
--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { homeController } from "./controllers/home-controller.js";
 import { stationController } from "./controllers/station-controller.js";
+import { authController } from "./controllers/auth-controller.js";
 
 export const router = express.Router();
 
@@ -19,6 +20,13 @@ router.post("/dashboard/add", dashboardController.addStation);
 // Station page
 router.get("/station/:id", stationController.index);
 router.post("/station/:id/add", stationController.addReading);
+
+// Authentication
+router.get("/login", authController.login);
+router.get("/register", authController.register);
+// router.post("/register", authController.registerUser);
+// router.post("/authenticate", authController.authenticate);
+// router.get("/logout", authController.logout);
 
 // About page
 router.get("/about", aboutController.index);
