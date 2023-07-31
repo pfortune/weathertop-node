666a43eedbbeae752b16661a6a47dba9413681b1
Peter Fortune <pfortune@gmail.com>
2023-07-31 14:17:48 +0100

    Added fade out after 5 secconds to Flash success  messages

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -18,34 +18,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  const deleteDialog = document.querySelector('#confirmDeleteDialog');
-  const deleteLinks = document.querySelectorAll('#delete');
-  const confirmButton = document.querySelector('#confirmDelete');
-  const deleteForm = document.querySelector('#deleteForm');
+  const flashMessage = document.getElementById('flash_message');
 
-  let deleteUrl;
-
-  deleteLinks.forEach((deleteLink) => {
-      deleteLink.addEventListener('click', (e) => {
-          e.preventDefault();  // Stop the link from navigating anywhere
-          deleteDialog.showModal();  // This function is native to the <dialog> element
-          deleteUrl = deleteLink.dataset.link; // Get the URL from the data-link attribute
-      });
-  });
-
-  deleteForm.addEventListener('submit', (e) => {
-      if (!deleteUrl) {
-          e.preventDefault();  // Prevent the form from being submitted
-      } else {
-          deleteForm.action = deleteUrl;  // Set the action of the form to the delete URL
-      }
-  });
-
-  // Get the cancel button
-  const cancelButton = document.querySelector('#cancelDelete');
-
-  // Close the dialog when the cancel button is clicked
-  cancelButton.addEventListener('click', () => {
-      deleteDialog.close();
-  });
+  if (flashMessage) {
+    setTimeout(function() {
+      let opacity = 1;
+      const timer = setInterval(function() {
+        if (opacity <= 0.1) {
+          clearInterval(timer);
+          flashMessage.style.display = 'none';
+        }
+        flashMessage.style.opacity = opacity;
+        flashMessage.style.filter = 'alpha(opacity=' + opacity * 100 + ")";
+        opacity -= opacity * 0.1;
+      }, 50);
+    }, 5000); // The fade-out starts after 5 seconds
+  }
 });
