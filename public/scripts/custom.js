cd38499d0da1aba5b1fbfde897f228b4ad6e74a8
Peter Fortune <pfortune@gmail.com>
2023-08-08 12:26:32 +0100

    Updated custom.js with dialog code

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -34,4 +34,35 @@ document.addEventListener("DOMContentLoaded", function () {
       }, 50);
     }, 5000); // The fade-out starts after 5 seconds
   }
+
+  const deleteDialog = document.querySelector('#confirmDeleteDialog');
+  const deleteLinks = document.querySelectorAll('#delete');
+  const confirmButton = document.querySelector('#confirmDelete');
+  const deleteForm = document.querySelector('#deleteForm');
+
+  let deleteUrl;
+
+  deleteLinks.forEach((deleteLink) => {
+      deleteLink.addEventListener('click', (e) => {
+          e.preventDefault();  // Stop the link from navigating anywhere
+          deleteDialog.showModal();  // This function is native to the <dialog> element
+          deleteUrl = deleteLink.dataset.link; // Get the URL from the data-link attribute
+      });
+  });
+
+  deleteForm.addEventListener('submit', (e) => {
+      if (!deleteUrl) {
+          e.preventDefault();  // Prevent the form from being submitted
+      } else {
+          deleteForm.action = deleteUrl;  // Set the action of the form to the delete URL
+      }
+  });
+
+  // Get the cancel button
+  const cancelButton = document.querySelector('#cancelDelete');
+
+  // Close the dialog when the cancel button is clicked
+  cancelButton.addEventListener('click', () => {
+      deleteDialog.close();
+  });
 });
