4d2f84e0b919e332d9ed00a676dae2bc5a8d9855
Peter Fortune <pfortune@gmail.com>
2023-08-11 13:00:52 +0100

    Fixed positioning of modal button. Added link to add reading on new station. Small fixes.

--- utils/max-min.js
+++ utils/max-min.js
@@ -1,18 +1,18 @@
 export function maxTemp(readings) {
-  return this.max(readings.map((reading) => reading.temperature));
+  return Math.max(...readings.map((reading) => reading.temperature));
 }
 export function minTemp(readings) {
-  return this.min(readings.map((reading) => reading.temperature));
+  return Math.min(...readings.map((reading) => reading.temperature));
 }
 export function maxWindSpeed(readings) {
-  return this.max(readings.map((reading) => reading.windSpeed));
+  return Math.max(...readings.map((reading) => reading.windSpeed));
 }
 export function minWindSpeed(readings) {
-  return this.min(readings.map((reading) => reading.windSpeed));
+  return Math.min(...readings.map((reading) => reading.windSpeed));
 }
 export function maxPressure(readings) {
-  return this.max(readings.map((reading) => reading.pressure));
+  return Math.max(...readings.map((reading) => reading.pressure));
 }
 export function minPressure(readings) {
-  return this.min(readings.map((reading) => reading.pressure));
+  return Math.min(...readings.map((reading) => reading.pressure));
 }
