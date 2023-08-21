d538ac6632c66e67932fb8a6506ed949b021cb56
Peter Fortune <pfortune@gmail.com>
2023-08-21 19:02:25 +0100

    Refactored requireAuth out of attachUser middleware

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { flash } from "./middleware/flash.js";
 import { attachUser } from "./middleware/attach-user.js";
 import { handle404Error } from "./middleware/handle-404.js";
+import { requireAuth } from "./middleware/require-auth.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { homeController } from "./controllers/home-controller.js";
@@ -11,30 +12,30 @@ import { authController } from "./controllers/auth-controller.js";
 export const router = express.Router();
 
 router.use(flash);
+router.use(attachUser);
 
 // Home page
 router.get("/", homeController.index);
 
 // Dashboard page
-router.get("/dashboard", attachUser(), dashboardController.index);
-router.post("/dashboard/add", attachUser(), dashboardController.addStation);
-router.post("/dashboard/:id/delete", attachUser(), dashboardController.deleteStation);
-// router.get("/dashboard/page/:page", dashboardController.index);
+router.get("/dashboard", requireAuth(), dashboardController.index);
+router.post("/dashboard/add", requireAuth(), dashboardController.addStation);
+router.post("/dashboard/:id/delete", requireAuth(), dashboardController.deleteStation);
 
 // Station page
-router.get("/station/:id", attachUser(), stationController.index);
-router.post("/station/:id/add", attachUser(), stationController.addReading);
-router.post("/station/:id/delete/:readingid", attachUser(), stationController.deleteReading);
-router.post("/station/:id/auto", attachUser(), stationController.addReadingFromAPI);
+router.get("/station/:id", requireAuth(), stationController.index);
+router.post("/station/:id/add", requireAuth(), stationController.addReading);
+router.post("/station/:id/delete/:readingid", requireAuth(), stationController.deleteReading);
+router.post("/station/:id/auto", requireAuth(), stationController.addReadingFromAPI);
 
 // Authentication
 router.get("/login", authController.login);
 router.get("/register", authController.register);
 router.post("/register", authController.registerUser);
 router.post("/authenticate", authController.authenticate);
-router.get("/logout", attachUser(), authController.logout);
-router.get("/account", attachUser(), authController.showAccount);
-router.post("/account", attachUser(), authController.updateAccount);
+router.get("/logout", requireAuth(), authController.logout);
+router.get("/account", requireAuth(), authController.showAccount);
+router.post("/account", requireAuth(), authController.updateAccount);
 
 // About page
 router.get("/about", aboutController.index);
